95555977aba562e27a6cf16c84631f84940684f4
Derek Wan <d.wan@icloud.com>
2023-05-28 22:53:12 +0800

    Finish lesson 5 (tailwind config) (#7)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: { 100: "#E2E2D5", 200: "#888883" },
+      },
+    },
   },
   plugins: [],
 };
